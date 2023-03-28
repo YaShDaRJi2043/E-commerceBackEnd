2d280c899255ce5957e1aebc68e73f9617ad2698
YaShDaRJi2043 <yashdarji1955@gmail.com>
2023-03-28 17:08:37 +0530

    changes

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ const Mendata = require("./insertdata/men");
 const womendata = require("./insertdata/women");
 const Homekitchendata = require("./insertdata/homekitchen");
 
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(port, () => {
-  console.log(`connection start on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`connection start on PORT ${PORT}`);
 });
 
 // defaultdata();
